609ceab544e290d3eb19443c3568025beadabda4
fmahadyBD <fmahady01@gmail.com>
2025-06-01 23:40:27 +0600

    Initial commit

---
+++ services-test/src/main/java/com/jk/services_test/ServicesTestApplication.java
@@ -0,0 +1,13 @@
+package com.jk.services_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesTestApplication.class, args);
+	}
+
+}
---
+++ services-test/src/test/java/com/jk/services_test/ServicesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.services_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
